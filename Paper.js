f69b1b030e703bf6df55b049b1794a36fb9bfd35
Ashnasmitha <75996347+Ashnasmitha@users.noreply.github.com>
2021-01-10 18:16:31 +0530

    Add files via upload

--- Paper.js
+++ Paper.js
@@ -7,9 +7,9 @@ class Paper
             friction:0.5,
             density:1.2
         }
-        this.body=Bodies.circle(100,550,20,options);
-        this.width=20;
-        this.height=20;
+        this.body=Bodies.circle(100,530,60,options);
+        this.width=60;
+        this.height=60;
         this.image=loadImage("paper.png");
 
         World.add(world,this.body);
@@ -19,7 +19,7 @@ class Paper
     display()
     { 
         imageMode (CENTER);
-        image (this.image,100,550,this.width,this.height);
+        image (this.image,100,530,this.width,this.height);
         
         
     }
